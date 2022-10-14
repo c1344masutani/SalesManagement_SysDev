5bffc3757b18a87f8ad01bf5a22451c2b8d49f02
Your Name <c1372@oic.jp>
2022-10-14 16:55:08 +0900

    form追加

---
+++ SalesManagement_SysDev/F_chumon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalesManagement_SysDev
+{
+    public partial class F_order_received : Form
+    {
+        public F_order_received()
+        {
+            InitializeComponent();
+        }
+
+        private void F_order_received_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
