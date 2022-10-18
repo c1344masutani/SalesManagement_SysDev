97b9ab889b35e93d7fa9c7d1dc2645dc8e83793d
maegawa tomoki <c1339@oic.jp>
2022-10-18 17:20:10 +0900

    商品フォームのボタン配置

--- SalesManagement_SysDev/F_product.Designer.cs
+++ SalesManagement_SysDev/F_product.Designer.cs
@@ -29,12 +29,362 @@ namespace SalesManagement_SysDev
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.dataGridViewDsp = new System.Windows.Forms.DataGridView();
+            this.PrFlag = new System.Windows.Forms.CheckBox();
+            this.textBoxFAX = new System.Windows.Forms.TextBox();
+            this.textBoxHidden = new System.Windows.Forms.TextBox();
+            this.PrModelNumberTextBox = new System.Windows.Forms.Label();
+            this.ScIDTextBox = new System.Windows.Forms.Label();
+            this.textBoxSalesOfficeID = new System.Windows.Forms.TextBox();
+            this.textBoxTel = new System.Windows.Forms.TextBox();
+            this.textBoxPostnumber = new System.Windows.Forms.TextBox();
+            this.textBoxClientName = new System.Windows.Forms.TextBox();
+            this.textBoxClientID = new System.Windows.Forms.TextBox();
+            this.PrSafetyStockTextBox = new System.Windows.Forms.Label();
+            this.PriceTextBox = new System.Windows.Forms.Label();
+            this.PrNameTextBox = new System.Windows.Forms.Label();
+            this.MaIDTextBox = new System.Windows.Forms.Label();
+            this.PrIDTextBox = new System.Windows.Forms.Label();
+            this.Regester_button = new System.Windows.Forms.Button();
+            this.Update_button = new System.Windows.Forms.Button();
+            this.Delete_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PrColorTextBox = new System.Windows.Forms.Label();
+            this.PrReleaseDateTextBox = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PrHiddenTextBox = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDsp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(872, 82);
+            this.buttonClear.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(80, 27);
+            this.buttonClear.TabIndex = 68;
+            this.buttonClear.Text = "入力クリア";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewDsp
+            // 
+            this.dataGridViewDsp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewDsp.Location = new System.Drawing.Point(11, 414);
+            this.dataGridViewDsp.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewDsp.Name = "dataGridViewDsp";
+            this.dataGridViewDsp.RowHeadersWidth = 62;
+            this.dataGridViewDsp.RowTemplate.Height = 27;
+            this.dataGridViewDsp.Size = new System.Drawing.Size(1083, 262);
+            this.dataGridViewDsp.TabIndex = 67;
+            // 
+            // PrFlag
+            // 
+            this.PrFlag.AutoSize = true;
+            this.PrFlag.Location = new System.Drawing.Point(468, 287);
+            this.PrFlag.Margin = new System.Windows.Forms.Padding(2);
+            this.PrFlag.Name = "PrFlag";
+            this.PrFlag.Size = new System.Drawing.Size(121, 19);
+            this.PrFlag.TabIndex = 66;
+            this.PrFlag.Text = "商品管理フラグ";
+            this.PrFlag.UseVisualStyleBackColor = true;
+            // 
+            // textBoxFAX
+            // 
+            this.textBoxFAX.Location = new System.Drawing.Point(657, 218);
+            this.textBoxFAX.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxFAX.Name = "textBoxFAX";
+            this.textBoxFAX.Size = new System.Drawing.Size(81, 22);
+            this.textBoxFAX.TabIndex = 65;
+            // 
+            // textBoxHidden
+            // 
+            this.textBoxHidden.Location = new System.Drawing.Point(674, 316);
+            this.textBoxHidden.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxHidden.Name = "textBoxHidden";
+            this.textBoxHidden.Size = new System.Drawing.Size(81, 22);
+            this.textBoxHidden.TabIndex = 64;
+            // 
+            // PrModelNumberTextBox
+            // 
+            this.PrModelNumberTextBox.AutoSize = true;
+            this.PrModelNumberTextBox.Location = new System.Drawing.Point(465, 325);
+            this.PrModelNumberTextBox.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.PrModelNumberTextBox.Name = "PrModelNumberTextBox";
+            this.PrModelNumberTextBox.Size = new System.Drawing.Size(37, 15);
+            this.PrModelNumberTextBox.TabIndex = 63;
+            this.PrModelNumberTextBox.Text = "型番";
+            // 
+            // ScIDTextBox
+            // 
+            this.ScIDTextBox.AutoSize = true;
+            this.ScIDTextBox.Location = new System.Drawing.Point(468, 218);
+            this.ScIDTextBox.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.ScIDTextBox.Name = "ScIDTextBox";
+            this.ScIDTextBox.Size = new System.Drawing.Size(66, 15);
+            this.ScIDTextBox.TabIndex = 62;
+            this.ScIDTextBox.Text = "子分類ID";
+            // 
+            // textBoxSalesOfficeID
+            // 
+            this.textBoxSalesOfficeID.Location = new System.Drawing.Point(225, 216);
+            this.textBoxSalesOfficeID.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxSalesOfficeID.Name = "textBoxSalesOfficeID";
+            this.textBoxSalesOfficeID.Size = new System.Drawing.Size(81, 22);
+            this.textBoxSalesOfficeID.TabIndex = 61;
+            // 
+            // textBoxTel
+            // 
+            this.textBoxTel.Location = new System.Drawing.Point(657, 176);
+            this.textBoxTel.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxTel.Name = "textBoxTel";
+            this.textBoxTel.Size = new System.Drawing.Size(81, 22);
+            this.textBoxTel.TabIndex = 60;
+            // 
+            // textBoxPostnumber
+            // 
+            this.textBoxPostnumber.Location = new System.Drawing.Point(225, 322);
+            this.textBoxPostnumber.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxPostnumber.Name = "textBoxPostnumber";
+            this.textBoxPostnumber.Size = new System.Drawing.Size(81, 22);
+            this.textBoxPostnumber.TabIndex = 58;
+            // 
+            // textBoxClientName
+            // 
+            this.textBoxClientName.Location = new System.Drawing.Point(225, 268);
+            this.textBoxClientName.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxClientName.Name = "textBoxClientName";
+            this.textBoxClientName.Size = new System.Drawing.Size(81, 22);
+            this.textBoxClientName.TabIndex = 57;
+            // 
+            // textBoxClientID
+            // 
+            this.textBoxClientID.Location = new System.Drawing.Point(225, 163);
+            this.textBoxClientID.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxClientID.Name = "textBoxClientID";
+            this.textBoxClientID.ReadOnly = true;
+            this.textBoxClientID.Size = new System.Drawing.Size(81, 22);
+            this.textBoxClientID.TabIndex = 56;
+            // 
+            // PrSafetyStockTextBox
+            // 
+            this.PrSafetyStockTextBox.AutoSize = true;
+            this.PrSafetyStockTextBox.Location = new System.Drawing.Point(468, 176);
+            this.PrSafetyStockTextBox.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.PrSafetyStockTextBox.Name = "PrSafetyStockTextBox";
+            this.PrSafetyStockTextBox.Size = new System.Drawing.Size(82, 15);
+            this.PrSafetyStockTextBox.TabIndex = 55;
+            this.PrSafetyStockTextBox.Text = "安全在庫数";
+            // 
+            // PriceTextBox
+            // 
+            this.PriceTextBox.AutoSize = true;
+            this.PriceTextBox.Location = new System.Drawing.Point(20, 322);
+            this.PriceTextBox.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.PriceTextBox.Name = "PriceTextBox";
+            this.PriceTextBox.Size = new System.Drawing.Size(37, 15);
+            this.PriceTextBox.TabIndex = 53;
+            this.PriceTextBox.Text = "価格";
+            // 
+            // PrNameTextBox
+            // 
+            this.PrNameTextBox.AutoSize = true;
+            this.PrNameTextBox.Location = new System.Drawing.Point(20, 268);
+            this.PrNameTextBox.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.PrNameTextBox.Name = "PrNameTextBox";
+            this.PrNameTextBox.Size = new System.Drawing.Size(52, 15);
+            this.PrNameTextBox.TabIndex = 52;
+            this.PrNameTextBox.Text = "商品名";
+            // 
+            // MaIDTextBox
+            // 
+            this.MaIDTextBox.AutoSize = true;
+            this.MaIDTextBox.Location = new System.Drawing.Point(20, 218);
+            this.MaIDTextBox.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.MaIDTextBox.Name = "MaIDTextBox";
+            this.MaIDTextBox.Size = new System.Drawing.Size(54, 15);
+            this.MaIDTextBox.TabIndex = 51;
+            this.MaIDTextBox.Text = "メーカID";
+            // 
+            // PrIDTextBox
+            // 
+            this.PrIDTextBox.AutoSize = true;
+            this.PrIDTextBox.Location = new System.Drawing.Point(20, 169);
+            this.PrIDTextBox.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.PrIDTextBox.Name = "PrIDTextBox";
+            this.PrIDTextBox.Size = new System.Drawing.Size(51, 15);
+            this.PrIDTextBox.TabIndex = 50;
+            this.PrIDTextBox.Text = "顧客ID";
+            // 
+            // Regester_button
+            // 
+            this.Regester_button.Location = new System.Drawing.Point(44, 106);
+            this.Regester_button.Margin = new System.Windows.Forms.Padding(2);
+            this.Regester_button.Name = "Regester_button";
+            this.Regester_button.Size = new System.Drawing.Size(60, 30);
+            this.Regester_button.TabIndex = 49;
+            this.Regester_button.Text = "追加";
+            this.Regester_button.UseVisualStyleBackColor = true;
+            // 
+            // Update_button
+            // 
+            this.Update_button.Location = new System.Drawing.Point(245, 106);
+            this.Update_button.Margin = new System.Windows.Forms.Padding(2);
+            this.Update_button.Name = "Update_button";
+            this.Update_button.Size = new System.Drawing.Size(60, 30);
+            this.Update_button.TabIndex = 48;
+            this.Update_button.Text = "更新";
+            this.Update_button.UseVisualStyleBackColor = true;
+            // 
+            // Delete_button
+            // 
+            this.Delete_button.Location = new System.Drawing.Point(443, 106);
+            this.Delete_button.Margin = new System.Windows.Forms.Padding(2);
+            this.Delete_button.Name = "Delete_button";
+            this.Delete_button.Size = new System.Drawing.Size(60, 30);
+            this.Delete_button.TabIndex = 47;
+            this.Delete_button.Text = "削除";
+            this.Delete_button.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(262, 49);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 46;
+            this.label1.Text = "顧客管理";
+            // 
+            // PrColorTextBox
+            // 
+            this.PrColorTextBox.AutoSize = true;
+            this.PrColorTextBox.Location = new System.Drawing.Point(471, 362);
+            this.PrColorTextBox.Name = "PrColorTextBox";
+            this.PrColorTextBox.Size = new System.Drawing.Size(22, 15);
+            this.PrColorTextBox.TabIndex = 69;
+            this.PrColorTextBox.Text = "色";
+            // 
+            // PrReleaseDateTextBox
+            // 
+            this.PrReleaseDateTextBox.AutoSize = true;
+            this.PrReleaseDateTextBox.Location = new System.Drawing.Point(809, 176);
+            this.PrReleaseDateTextBox.Name = "PrReleaseDateTextBox";
+            this.PrReleaseDateTextBox.Size = new System.Drawing.Size(52, 15);
+            this.PrReleaseDateTextBox.TabIndex = 70;
+            this.PrReleaseDateTextBox.Text = "発売日";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(809, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 15);
+            this.label2.TabIndex = 71;
+            // 
+            // PrHiddenTextBox
+            // 
+            this.PrHiddenTextBox.AutoSize = true;
+            this.PrHiddenTextBox.Location = new System.Drawing.Point(815, 223);
+            this.PrHiddenTextBox.Name = "PrHiddenTextBox";
+            this.PrHiddenTextBox.Size = new System.Drawing.Size(82, 15);
+            this.PrHiddenTextBox.TabIndex = 72;
+            this.PrHiddenTextBox.Text = "非表示理由";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(674, 354);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(81, 22);
+            this.textBox1.TabIndex = 73;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(940, 176);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 74;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(940, 223);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 75;
+            // 
+            // F_product
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1164, 705);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.PrHiddenTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PrReleaseDateTextBox);
+            this.Controls.Add(this.PrColorTextBox);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.dataGridViewDsp);
+            this.Controls.Add(this.PrFlag);
+            this.Controls.Add(this.textBoxFAX);
+            this.Controls.Add(this.textBoxHidden);
+            this.Controls.Add(this.PrModelNumberTextBox);
+            this.Controls.Add(this.ScIDTextBox);
+            this.Controls.Add(this.textBoxSalesOfficeID);
+            this.Controls.Add(this.textBoxTel);
+            this.Controls.Add(this.textBoxPostnumber);
+            this.Controls.Add(this.textBoxClientName);
+            this.Controls.Add(this.textBoxClientID);
+            this.Controls.Add(this.PrSafetyStockTextBox);
+            this.Controls.Add(this.PriceTextBox);
+            this.Controls.Add(this.PrNameTextBox);
+            this.Controls.Add(this.MaIDTextBox);
+            this.Controls.Add(this.PrIDTextBox);
+            this.Controls.Add(this.Regester_button);
+            this.Controls.Add(this.Update_button);
+            this.Controls.Add(this.Delete_button);
+            this.Controls.Add(this.label1);
+            this.Name = "F_product";
             this.Text = "product";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDsp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.DataGridView dataGridViewDsp;
+        private System.Windows.Forms.CheckBox PrFlag;
+        private System.Windows.Forms.TextBox textBoxFAX;
+        private System.Windows.Forms.TextBox textBoxHidden;
+        private System.Windows.Forms.Label PrModelNumberTextBox;
+        private System.Windows.Forms.Label ScIDTextBox;
+        private System.Windows.Forms.TextBox textBoxSalesOfficeID;
+        private System.Windows.Forms.TextBox textBoxTel;
+        private System.Windows.Forms.TextBox textBoxPostnumber;
+        private System.Windows.Forms.TextBox textBoxClientName;
+        private System.Windows.Forms.TextBox textBoxClientID;
+        private System.Windows.Forms.Label PrSafetyStockTextBox;
+        private System.Windows.Forms.Label PriceTextBox;
+        private System.Windows.Forms.Label PrNameTextBox;
+        private System.Windows.Forms.Label MaIDTextBox;
+        private System.Windows.Forms.Label PrIDTextBox;
+        private System.Windows.Forms.Button Regester_button;
+        private System.Windows.Forms.Button Update_button;
+        private System.Windows.Forms.Button Delete_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label PrColorTextBox;
+        private System.Windows.Forms.Label PrReleaseDateTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label PrHiddenTextBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
