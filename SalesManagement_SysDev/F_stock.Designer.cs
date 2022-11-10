f0d0f9ef6367f5ecd063625f06e9be4dacdc409c
1C34KW <c1055@oic.jp>
2022-11-10 16:08:02 +0900

    a

--- SalesManagement_SysDev/F_stock.Designer.cs
+++ SalesManagement_SysDev/F_stock.Designer.cs
@@ -29,12 +29,207 @@ namespace SalesManagement_SysDev
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxStockID = new System.Windows.Forms.TextBox();
+            this.textBoxProductID = new System.Windows.Forms.TextBox();
+            this.textBoxStQuantity = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("HGS明朝E", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(123)))), ((int)(((byte)(68)))));
+            this.label1.Location = new System.Drawing.Point(206, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(305, 56);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "在 庫 管 理";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("HGS明朝E", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(123)))), ((int)(((byte)(68)))));
+            this.label2.Location = new System.Drawing.Point(150, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 40);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "在庫ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("HGS明朝E", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(123)))), ((int)(((byte)(68)))));
+            this.label3.Location = new System.Drawing.Point(151, 279);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 36);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "商品ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("HGS明朝E", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(123)))), ((int)(((byte)(68)))));
+            this.label4.Location = new System.Drawing.Point(662, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 40);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "在庫数";
+            // 
+            // textBoxStockID
+            // 
+            this.textBoxStockID.Location = new System.Drawing.Point(352, 175);
+            this.textBoxStockID.Multiline = true;
+            this.textBoxStockID.Name = "textBoxStockID";
+            this.textBoxStockID.Size = new System.Drawing.Size(206, 51);
+            this.textBoxStockID.TabIndex = 4;
+            // 
+            // textBoxProductID
+            // 
+            this.textBoxProductID.Location = new System.Drawing.Point(352, 279);
+            this.textBoxProductID.Multiline = true;
+            this.textBoxProductID.Name = "textBoxProductID";
+            this.textBoxProductID.Size = new System.Drawing.Size(206, 51);
+            this.textBoxProductID.TabIndex = 5;
+            // 
+            // textBoxStQuantity
+            // 
+            this.textBoxStQuantity.Location = new System.Drawing.Point(854, 175);
+            this.textBoxStQuantity.Multiline = true;
+            this.textBoxStQuantity.Name = "textBoxStQuantity";
+            this.textBoxStQuantity.Size = new System.Drawing.Size(206, 51);
+            this.textBoxStQuantity.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(285, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 18);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "必須";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(281, 297);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 18);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "必須";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(804, 197);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 18);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "必須";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(157, 394);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(1700, 550);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("HGS明朝E", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(123)))), ((int)(((byte)(68)))));
+            this.button1.Location = new System.Drawing.Point(793, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 56);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "検索";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("HGS明朝E", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(123)))), ((int)(((byte)(68)))));
+            this.button2.Location = new System.Drawing.Point(1008, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 56);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "更新";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("HGS明朝E", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(123)))), ((int)(((byte)(68)))));
+            this.button3.Location = new System.Drawing.Point(1703, 26);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 56);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "戻る";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // F_stock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.ClientSize = new System.Drawing.Size(1898, 1024);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxStQuantity);
+            this.Controls.Add(this.textBoxProductID);
+            this.Controls.Add(this.textBoxStockID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "F_stock";
             this.Text = "stock";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxStockID;
+        private System.Windows.Forms.TextBox textBoxProductID;
+        private System.Windows.Forms.TextBox textBoxStQuantity;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
