510e68e255a987e933526b4f3237153b4b9363fc
maegawa tomoki <c1339@oic.jp>
2022-11-11 16:14:25 +0900

    注文管理

--- SalesManagement_SysDev/F_tyuumon.cs
+++ SalesManagement_SysDev/F_tyuumon.cs
@@ -16,5 +16,10 @@ namespace SalesManagement_SysDev
         {
             InitializeComponent();
         }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
