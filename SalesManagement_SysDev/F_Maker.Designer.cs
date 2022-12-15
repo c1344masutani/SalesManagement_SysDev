f592d02c6c4d66af632312d2ec7f67f6588305b4
maegawa tomoki <c1339@oic.jp>
2022-12-15 15:36:08 +0900

    maker

---
+++ SalesManagement_SysDev/F_Maker.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace SalesManagement_SysDev
+{
+    partial class F_Maker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Regester_button = new System.Windows.Forms.Button();
+            this.Update_button = new System.Windows.Forms.Button();
+            this.Search_button = new System.Windows.Forms.Button();
+            this.back_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::SalesManagement_SysDev.Properties.Resources.S__33333256;
+            this.pictureBox1.InitialImage = global::SalesManagement_SysDev.Properties.Resources.S__33333256;
+            this.pictureBox1.Location = new System.Drawing.Point(21, 21);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(117, 78);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 91;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("HGS明朝E", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(123)))), ((int)(((byte)(87)))));
+            this.label1.Location = new System.Drawing.Point(186, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(319, 47);
+            this.label1.TabIndex = 92;
+            this.label1.Text = "メ ー カ 管 理";
+            // 
+            // Regester_button
+            // 
+            this.Regester_button.Font = new System.Drawing.Font("HGS明朝E", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Regester_button.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(123)))), ((int)(((byte)(68)))));
+            this.Regester_button.Location = new System.Drawing.Point(616, 26);
+            this.Regester_button.Margin = new System.Windows.Forms.Padding(2);
+            this.Regester_button.Name = "Regester_button";
+            this.Regester_button.Size = new System.Drawing.Size(186, 42);
+            this.Regester_button.TabIndex = 93;
+            this.Regester_button.Text = "新規追加";
+            this.Regester_button.UseVisualStyleBackColor = true;
+            // 
+            // Update_button
+            // 
+            this.Update_button.Font = new System.Drawing.Font("HGS明朝E", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Update_button.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(123)))), ((int)(((byte)(68)))));
+            this.Update_button.Location = new System.Drawing.Point(899, 26);
+            this.Update_button.Margin = new System.Windows.Forms.Padding(2);
+            this.Update_button.Name = "Update_button";
+            this.Update_button.Size = new System.Drawing.Size(113, 42);
+            this.Update_button.TabIndex = 94;
+            this.Update_button.Text = "更新";
+            this.Update_button.UseVisualStyleBackColor = true;
+            // 
+            // Search_button
+            // 
+            this.Search_button.Font = new System.Drawing.Font("HGS明朝E", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Search_button.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(123)))), ((int)(((byte)(68)))));
+            this.Search_button.Location = new System.Drawing.Point(1073, 28);
+            this.Search_button.Margin = new System.Windows.Forms.Padding(2);
+            this.Search_button.Name = "Search_button";
+            this.Search_button.Size = new System.Drawing.Size(113, 42);
+            this.Search_button.TabIndex = 95;
+            this.Search_button.Text = "検索";
+            this.Search_button.UseVisualStyleBackColor = true;
+            // 
+            // back_button
+            // 
+            this.back_button.Font = new System.Drawing.Font("HGS明朝E", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.back_button.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(123)))), ((int)(((byte)(87)))));
+            this.back_button.Location = new System.Drawing.Point(1778, 28);
+            this.back_button.Margin = new System.Windows.Forms.Padding(2);
+            this.back_button.Name = "back_button";
+            this.back_button.Size = new System.Drawing.Size(112, 42);
+            this.back_button.TabIndex = 96;
+            this.back_button.Text = "戻る";
+            this.back_button.UseVisualStyleBackColor = true;
+            // 
+            // F_Maker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.ClientSize = new System.Drawing.Size(1924, 1033);
+            this.Controls.Add(this.back_button);
+            this.Controls.Add(this.Search_button);
+            this.Controls.Add(this.Update_button);
+            this.Controls.Add(this.Regester_button);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "F_Maker";
+            this.Text = "F_Maker";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Regester_button;
+        private System.Windows.Forms.Button Update_button;
+        private System.Windows.Forms.Button Search_button;
+        private System.Windows.Forms.Button back_button;
+    }
+}
